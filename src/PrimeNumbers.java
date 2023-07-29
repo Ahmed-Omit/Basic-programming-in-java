a51e26fb12e9fff4dab0f89b7272940b4285ce74
Ahmed-Omit <139171743+Ahmed-Omit@users.noreply.github.com>
2023-07-29 10:45:54 +0600

    code

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
 public class PrimeNumbers {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n, i, j, b;
         System.out.print(" Enter the value of n : ");
         n = s.nextInt();
-        for (i = 1; i <= n; i++)
-        {
+        for (i = 1; i <= n; i++) {
             b = 0;
-            for (j = 1; j <= i; j++)
-            {
-                if (i % j == 0)
-                {
+            for (j = 1; j <= i; j++) {
+                if (i % j == 0) {
                     b = b + 1;
                 }
             }
